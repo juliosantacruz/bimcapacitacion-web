e10c81b039a318a0af4bafbc32405f4209b72649
Julio Santacruz <julio.santacruzh@gmail.com>
2024-01-08 21:44:18 -0800

    head config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations:[],
+  integrations:[sitemap(), partytown()],
   site: 'https://juliosantacruz.dev',
   base: '/bimcapacitacion-web',
 });
