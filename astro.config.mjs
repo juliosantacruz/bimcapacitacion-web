edd0a7fd8d5c3f8ccefbb6d2f9094b8cf0ba33b0
Julio Santacruz <julio.santacruzh@gmail.com>
2024-03-16 12:13:40 -0700

    domain test

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,6 @@ export default defineConfig({
       include: ["**/react/*"],
     }),
   ],
-  site: "https://www.juliosantacruz.dev",
-  base: "/bimcapacitacion",
+  site: "https://www.bimcapacitacion.com",
+  base: "/",
 });
